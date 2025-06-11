53de2ef014425a6cdeda59110c2ca68b51c215da
apptcion <apptcion@gmail.com>
2025-06-11 20:57:07 +0900

    eyes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,31 +18,31 @@ import {useEffect} from 'react'
 
 export default function Home() {
 
-  // useEffect(() => {
-  //   if(getCookie('ticket') === undefined){
-  //     setCookie('from','http://chess.apptcion.site');
-  //     console.log("토큰 없음", getCookie('ticket'))
-  //     location.href = 'https://apptcion.site/filter';
-  //   }else{
-  //     fetch('https://apptcion.site/isValid',{
-  //       method:'POST',
-  //       headers:{
-  //         'Content-Type':'application/json'
-  //       },
-  //       body:JSON.stringify({
-  //         ticket:getCookie('ticket'),
-  //       })
-  //     }).then((response) => {
-  //         if(response.ok) return response.json();
-  //     }).then((data) => {
-  //       if(!data){
-  //         setCookie('from','http://chess.apptcion.site/');
-  //         location.href = 'https://apptcion.site/filter';
-  //         console.log("토큰 잘못됨", getCookie('ticket'))
-  //       }
-  //     })
-  //   }
-  // })
+  useEffect(() => {
+    if(getCookie('ticket') === undefined){
+      setCookie('from','http://chess.apptcion.site');
+      console.log("토큰 없음", getCookie('ticket'))
+      location.href = 'https://apptcion.site/filter';
+    }else{
+      fetch('https://apptcion.site/isValid',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+          ticket:getCookie('ticket'),
+        })
+      }).then((response) => {
+          if(response.ok) return response.json();
+      }).then((data) => {
+        if(!data){
+          setCookie('from','http://chess.apptcion.site/');
+          location.href = 'https://apptcion.site/filter';
+          console.log("토큰 잘못됨", getCookie('ticket'))
+        }
+      })
+    }
+  })
 
   return (
     <div className={` ${styles.CardContainer} `}>
